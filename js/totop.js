1e01bbb321c9246541ebb523f82345574e4c668a
zhangruyn <2797541588@qq.com>
2016-01-07 11:43:24 +0800

    Site updated: 2016-01-07 11:43:23

--- js/totop.js
+++ js/totop.js
@@ -1,7 +1,7 @@
 (function($) {
 	// When to show the scroll link
 	// higher number = scroll link appears further down the page
-	var upperLimit = 1200;
+	var upperLimit = 700;
 
 	// Our scroll link element
 	var scrollElem = $('#totop');
